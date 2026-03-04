e4a112f8212a653402fd50a2f6a744c83d82580b
baban-afnan <shafiumuhd114@gmail.com>
2026-03-04 15:03:14 +0100

    done

--- resources/views/emails/two_factor_code.blade.php
+++ resources/views/emails/two_factor_code.blade.php
@@ -12,6 +12,9 @@ If you did not request this code, please ignore this email.
 - Do not share this code with anyone
 - If you did not request this code, please ignore this email and secure your account
 
+Note: do not share this code with anyone arewasmart will never ask for your login or OTP
+be wise always
+
 Thanks,<br>
 {{ config('app.name') }}
 </x-mail::message>
